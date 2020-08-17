30f217bbb6f6288acebafb5174ac8e08c4e84a38
RaidenDilan <raiden18@me.com>
2020-08-17 02:31:37 +0100

    Deploying - Heroku

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ module.exports = {
     APP_NAME: 'NODE-REACT-AWS',
     API: 'https://react-node-aws-api.herokuapp.com/api',
     PRODUCTION: true,
-    DOMAIN: 'https://react-node-aws.herokuapp.com/',
+    DOMAIN: 'https://react-node-aws.herokuapp.com',
     FB_APP_ID: '707G75DS76G98SD5GF6G'
   }
 };
